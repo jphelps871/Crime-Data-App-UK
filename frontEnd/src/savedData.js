0fd9e9467ebc7d529387fe3832ceb1a42e78552e
Jamie Phelps <james.phelps1995@live.com>
2021-02-20 10:52:15 +0000

    get user access to 'saved data' page

--- frontEnd/src/savedData.js
+++ frontEnd/src/savedData.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const SavedData = () => {
+  const [loading, setLoading] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [response, setResponse] = useState("");
+
+  useEffect(async () => {
+    try {
+      const response = await fetch("/api/user/checkToken");
+      const data = await response.text();
+      setResponse(data);
+    } catch (err) {
+      console.log(err);
+      setResponse("need to be logged in");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   return (
     <div className="pages">
-      <h2>Your saved data</h2>
+      {loading ? <h2>Loading...</h2> : <h2>{response}</h2>}
     </div>
   );
 };
